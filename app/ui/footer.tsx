cc3111a9c122712126cab2725fa86d41fa736f03
ciweisoon <ciweisoon@gmail.com>
2024-03-14 11:19:52 +0800

    update footer responsive view

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -4,13 +4,14 @@ import Footer_Logo from "@/public/restaurant/footer-logo.png";
 
 const Footer = () => {
     return (
-        <footer className="grid grid-cols-[200px_minmax(900px,_1fr)] gap-4">
+        <footer className="relative isolate px-10 py-16 lg:px-8 my-5 md:grid grid-cols-[1fr_3fr] gap-auto">
             <Image
                 src={Footer_Logo}
                 alt="Little Lemon Restaurant Logo"
                 width={100}
+                className="hidden md:block"
             />
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 grid-rows-1 gap-auto sm:grid-cols-3 grid-rows-1">
                 <div>
                     <h1>Navigation</h1>
                     <Link className="block hover:text-gray-400" href="/">Home</Link>
@@ -31,7 +32,6 @@ const Footer = () => {
                     <Link className="block hover:text-gray-400" href="https://twitter.com/littlelemonrhw" target="_blank">Twitter</Link>
                     <Link className="block hover:text-gray-400" href="https://www.linkedin.com/company/littlelemon/" target="_blank">LinkedIn</Link>
                 </div>
-                <div></div>
             </div>
         </footer>
     )
